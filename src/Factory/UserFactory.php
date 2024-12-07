10418cbd8cb43f5b2cb46c51b3aeb22d74569288
Clémentine <clementine.bertrand@lilo.org>
2024-12-07 17:23:37 +0100

    Ajout des entités et fixtures

---
+++ src/Factory/UserFactory.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<User>
+ */
+final class UserFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct(private readonly UserPasswordHasherInterface $hasher)
+    {
+        parent::__construct();
+    }
+
+    public static function class(): string
+    {
+        return User::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'password' => $this->hasher->hashPassword(new User(), 'password'),
+            'roles' => ['ROLE_USER'],
+            'username' => self::faker()->userName(),
+            'zipcode' => self::faker()->randomElement(['19032', '67198', '07192', '35185', '33068', '32003', '27743', '94314', '53159', '69001']),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(User $user): void {})
+        ;
+    }
+}
