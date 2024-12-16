aca270f701375a4127fa5e773369a6e3c067183c
Clémentine <clementine.bertrand@lilo.org>
2024-12-16 12:22:47 +0100

    Bug fix

--- src/Controller/OpenWeatherController.php
+++ src/Controller/OpenWeatherController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
@@ -15,21 +19,24 @@ class OpenWeatherController extends AbstractController
 {
     #[Route('api/meteo', name: 'api_currentLocation_weather', methods: ['GET'])]
     #[IsGranted('ROLE_USER', message:'Vous devez être connectés pour accéder à cette page.')]
-    public function getWeatherByZipcode(CacheInterface $cache, HttpClientInterface $httpClient): JsonResponse
+    public function getWeatherByZipcode(TokenStorageInterface $tokenStorageInterface, JWTTokenManagerInterface $jwtManager, UserRepository $userRepository, CacheInterface $cache, HttpClientInterface $httpClient): JsonResponse
     {
-        $zipcode = 69290;
+        $decodedJwtToken = $jwtManager->decode($tokenStorageInterface->getToken());
+        $currentUser = $userRepository->getUserByName($decodedJwtToken['username']);
+        $zipcode = $currentUser->getZipcode();
         $cacheKey = 'weather_' . $zipcode;
+        $apiKey = $_ENV['API_KEY'];
 
-        $weatherData = $cache->get($cacheKey, function (ItemInterface $item) use ($httpClient, $zipcode) {
+        $weatherData = $cache->get($cacheKey, function (ItemInterface $item) use ($httpClient, $zipcode, $apiKey) {
             $item->expiresAfter(3600);
 
             try {
-                $curlResponse = $httpClient->request('GET', 'http://api.openweathermap.org/geo/1.0/zip?zip='. $zipcode . ',FR&appid=d349b955093f2e4e23d1e99a66784187');
+                $curlResponse = $httpClient->request('GET', 'http://api.openweathermap.org/geo/1.0/zip?zip='. $zipcode . ',FR&appid=' . $apiKey);
                 $coordinates = $curlResponse->toArray();
                 if (empty($coordinates)) {
                     throw new \Exception('Code postal inexistant.');
                 }   
-                $response = $httpClient->request('GET', 'https://api.openweathermap.org/data/2.5/weather?lat='. $coordinates['lat'] .'&lon='. $coordinates['lon'] .'&appid=d349b955093f2e4e23d1e99a66784187');
+                $response = $httpClient->request('GET', 'https://api.openweathermap.org/data/2.5/weather?lat='. $coordinates['lat'] .'&lon='. $coordinates['lon'] .'&appid=' . $apiKey);
                 return $response->toArray();
             }
             catch (\Exception $e) {
@@ -49,18 +56,19 @@ class OpenWeatherController extends AbstractController
     public function getWeatherByCity(string $city, CacheInterface $cache, HttpClientInterface $httpClient): JsonResponse
     {
         $cacheKey = 'weather_' . $city;
+        $apiKey = $_ENV['API_KEY'];
 
-        $weatherData = $cache->get($cacheKey, function (ItemInterface $item) use ($httpClient, $city) {
+        $weatherData = $cache->get($cacheKey, function (ItemInterface $item) use ($httpClient, $city, $apiKey) {
             $item->expiresAfter(3600);
 
             try {
-                $curlResponse = $httpClient->request('GET', 'http://api.openweathermap.org/geo/1.0/direct?q='. $city . ',FR&appid=d349b955093f2e4e23d1e99a66784187');
+                $curlResponse = $httpClient->request('GET', 'http://api.openweathermap.org/geo/1.0/direct?q='. $city . ',FR&appid=' . $apiKey);
                 $arrayResponse = $curlResponse->toArray();
                 if (empty($arrayResponse)) {
                     throw new \Exception('Aucune ville trouvée.');
                 }    
                 $coordinates = $arrayResponse[0];
-                $response = $httpClient->request('GET', 'https://api.openweathermap.org/data/2.5/weather?lat='. $coordinates['lat'] .'&lon='. $coordinates['lon'] .'&appid=d349b955093f2e4e23d1e99a66784187');
+                $response = $httpClient->request('GET', 'https://api.openweathermap.org/data/2.5/weather?lat='. $coordinates['lat'] .'&lon='. $coordinates['lon'] .'&appid=' . $apiKey);
                 return $response->toArray();
             } catch (\Exception $e) {
                 return [
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -48,13 +48,13 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
     //        ;
     //    }
 
-    //    public function findOneBySomeField($value): ?User
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function getUserByName($name): ?User
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.username = :name')
+            ->setParameter('name', $name)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
 }
